aab33bf7793f80ac441dde0691edecbad451ea59
frequel45 <154789727+frequel45@users.noreply.github.com>
2024-10-14 16:52:56 +0530

    Update page.tsx

--- src/app/about/page.tsx
+++ src/app/about/page.tsx
@@ -2,31 +2,10 @@
 
 export default function AboutPage() {
   return (
+    <div> About Page </div>
+     
     
-      <section className="text-center md: text-left p-4 md:p-8">
-      <h1 className="text-2xl md:text-4xl font-bold mb-2 md:mb-4">Welcome to our Generative AI Basics Application!</h1>
-    <p className="text-base md:text-lg mb-4">
-    Our platform is dedicated to demystifying Generative AI and making it accessible to everyone through insightful, easy-to-understand blog posts. Whether you're a curious beginner or a tech enthusiast, our blogs cover everything you need to know about the fascinating world of Generative AI.</p>
     
-    <h1 className="text-2xl md:text-4xl font-bold mb-2 md:mb-4">What We Offer:</h1>
-     <h2 className="text-xl md:text-2xl font-bold mb-1 md:mb-2">Foundational Knowledge: </h2>
-    <p className="text-base md:text-lg mb-4">Our blogs break down complex Generative AI concepts into simple, digestible content. Learn the basics of algorithms, neural networks, and how Generative AI works.</p>
-    
-    <h2 className="text-xl md:text-2xl font-bold mb-1 md:mb-2">Interactive Examples:</h2>
-    <p className="text-base md:text-lg mb-4"> See practical examples and real-life applications of Generative AI. From AI-generated art to music and text, our posts showcase how this technology is transforming various fields.</p>
-    
-    <h2 className="text-xl md:text-2xl font-bold mb-1 md:mb-2">Step-by-Step Guides:</h2>
-    <p className="text-base md:text-lg mb-4"> Follow along with our detailed guides to start your own Generative AI projects. We provide tips, tutorials, and best practices to help you get started.</p>
-    
-    <h2 className="text-xl md:text-2xl font-bold mb-1 md:mb-2">Expert Insights:</h2>
-    <p className="text-base md:text-lg mb-4">Gain insights from AI experts and enthusiasts. Our blog features guest posts and interviews with leading voices in the field of Generative AI.</p>
-    
-    <h2 className="text-xl md:text-2xl font-bold mb-1 md:mb-2">Our Mission:</h2>
-    
-    <p className="text-base md:text-lg mb-4">Our goal is to make Generative AI approachable and fun. We believe that understanding this technology can open up new creative possibilities and inspire innovation. Through our blogs, we aim to educate, engage, and empower our readers to explore and experiment with Generative AI.</p>
-    
-    <p className="text-base md:text-lg mb-4">Join us on a journey to unlock the potential of artificial intelligence. Dive into our blog and start learning today!</p>
-    </section>
     
   );
 }
